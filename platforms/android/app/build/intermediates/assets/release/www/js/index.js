71c8bbf39a61bb85756fa715aab83d21fb574982
Surajit Sadhukhan <surajit1992@live.com>
2019-07-25 12:54:23 +0530

    web app for first commit

---
+++ platforms/android/app/build/intermediates/assets/release/www/js/index.js
@@ -0,0 +1,10 @@
+if(window.history.length > 1) { document.getElementById("loadingDiv").hidden = true; }
+function onDeviceReady() {
+  if(window.history.length > 1) { navigator.app.exitApp(); }
+    if (navigator.connection.type == Connection.NONE) {
+      navigator.notification.alert('An internet connection is required to continue');
+    } else {
+      window.location="http://fresh365.co/";
+    }
+  }
+  document.addEventListener("deviceready", onDeviceReady, false);
