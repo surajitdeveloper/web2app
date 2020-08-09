c5a3686f5bf132d7be7a83f78e9397f494c3f9e5
Aashish Kumar <kumaraashish@facileconsulting.com>
2020-08-09 19:52:34 +0530

    new

--- apk.bat
+++ apk.bat
@@ -1,1 +1,4 @@
-cordova clean android && cordova build android --release && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ezmarket.keystore D:\web2app\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk ezmarket && jarsigner -verify -verbose -certs D:\web2app\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk && apksigner verify verifiy --verbose --print-certs D:\web2app\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk && zipalign -v 4 D:\web2app\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk D:\web2app\platforms\android\app\build\outputs\apk\release\rmarket.apk
+call cordova clean android 
+call cordova build android --release 
+call jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ezmarket.keystore D:\projects\web2app\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk ezmarket 
+call zipalign -v 4 D:\projects\web2app\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk D:\projects\web2app\platforms\android\app\build\outputs\apk\release\rmarket.apk
--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -4,7 +4,7 @@ function onDeviceReady() {
     if (navigator.connection.type == Connection.NONE) {
       navigator.notification.alert('An internet connection is required to continue');
     } else {
-      window.location="http://116.203.180.56/redirect.php?url=http://booksfair.in";
+      window.location="http://ba2z.in/";
     }
   }
   document.addEventListener("deviceready", onDeviceReady, false);
