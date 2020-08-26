4335e052f9c83048803927b96eb17f454bd8d7c8
Asheesh Thakur <asheesh@facileconsulting.com>
2020-08-26 13:35:11 +0530

    new

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -4,7 +4,7 @@ function onDeviceReady() {
     if (navigator.connection.type == Connection.NONE) {
       navigator.notification.alert('An internet connection is required to continue');
     } else {
-      window.location.href = "http://xpertzinfo.com";
+      window.location.href = "https://kolkatapg.in/";
     }
   }
   document.addEventListener("deviceready", onDeviceReady, false);
